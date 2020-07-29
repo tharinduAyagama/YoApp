261cca0f5b99cf309e719b8990794e9dfa5da539
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-07-29 19:01:46 +0530

    Navigation added

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StyleSheet, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 import Home from './screens/Home';
 import Chat from './screens/Chat';
@@ -7,13 +9,13 @@ import Register from './screens/Register';
 import AddFriend from './screens/AddFriend';
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" />
       {/* <Chat /> */}
       <Home />
       {/* <Register /> */}
       {/* <AddFriend /> */}
-    </>
+    </NavigationContainer>
   );
 };
 
